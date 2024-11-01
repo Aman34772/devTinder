04c2031a5ef473f541cba376fff803453e8cb249
Aman34772 <amankumar34772@gmail.com>
2024-11-01 16:49:20 +0530

    exploring get,post ,delete

--- homework.md
+++ homework.md
@@ -9,3 +9,17 @@ Install nodemon and update scripts inside package.json
 differnce between carret and tilda (^ and ~)
 what are dependencies and dev dependencies
 what is the use of "-g while installing npm install
+
+
+initialize git 
+gitignore
+create a remote repository on github
+push all code to remote origin
+
+play with routes and route extensions ex: /hello, /hello/2, /xyz
+
+Order of the routes matters
+install postman app and make a workspace and collection
+make a test api call
+
+make right logic to handle get ,post ,patch,delete API calls and test them on postman
--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => res.send("Namaste Akshay"));
-app.use("/hello", (req, res) => res.send("hello hello hello"));
+
+app.use("/user", (req, res) => res.send("hahhahahahahaha"));
+
+
+//this will only handle the GET call to /user
+app.get("/user", (req, res) => res.send("Hey user"));
+
+
+//this will match all the http method API calls to /test
+// app.use("/", (req, res) => {
+//     res.send("Namaste Akshay");
+// });
+
+app.post("/user",(req,res)=>{
+    //saving to the database
+    console.log("Hello jii save kr rha hu")
+    res.send("Hello to the database")
+});
+app.delete("/user",(req,res)=>res.send("deleted successfully"))
 app.use("/test", (req, res) => res.send("hello from the server"));
-app.get("/", (req, res) => res.send("Hey"));
 
 app.listen(7777, () => console.log("connected"));
